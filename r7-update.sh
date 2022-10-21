42396164dd66c58ff94f9abeb122d58a36ca0bf9
Procopii Smetanin <84850589+procoprobocop@users.noreply.github.com>
2022-10-21 17:39:17 +0500

    Update r7-update.sh

--- r7-update.sh
+++ r7-update.sh
@@ -5,3 +5,8 @@ sleep 5
 killall -ws 9 -u $USER editors_helper
 #скачиваем и устанавливаем без сохранения на диске новую версию r7-office
 dnf install https://download.r7-office.ru/centos/r7-office.rpm -y
+#добавляем скрипт в планировщик задач, где он будет выполняться каждое 1 число месяца
+crontab -l > crontab_new
+echo "0 0 1 * * root /usr/share/r7-update.sh" >> crontab_new
+crontab crontab_new
+rm crontab_new
